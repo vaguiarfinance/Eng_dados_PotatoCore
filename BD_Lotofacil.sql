9755af2bb164e3723e19700053ba5a01b2a2f53a
Vinicius Aguiar <114686680+vaguiarfinance@users.noreply.github.com>
2023-09-28 17:10:55 -0300

    Update BD_Lotofacil.sql

--- BD_Lotofacil.sql
+++ BD_Lotofacil.sql
@@ -11,6 +11,21 @@ CREATE TABLE Sorteios (
 CREATE TABLE NumerosSorteados (
     id serial PRIMARY KEY,
     sorteio_id integer REFERENCES Sorteios(id),
+    bola_1 INT,
+    bola_2 INT,
+    bola_3 INT,
+    bola_4 INT,
+    bola_5 INT,
+    bola_6 INT,
+    bola_7 INT,
+    bola_8 INT,
+    bola_9 INT,
+    bola_10 INT,
+    bola_11 INT,
+    bola_12 INT,
+    bola_13 INT,
+    bola_14 INT,
+    bola_15 INT,
     numero_sorteado integer NOT NULL,
     CONSTRAINT unique_numeros_sorteados UNIQUE (sorteio_id, numero_sorteado)
 );
